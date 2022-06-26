3891cfb2eeae27b4ce96700322feb4dcf7a1584d
nikstarling <rioskill@yandex.ru>
2022-06-27 01:54:57 +0300

    changed links from hardcoded to db stored

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class WordLinks(models.Model):
+    word = models.CharField(max_length=50)
+    href = models.CharField(max_length=150)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.templatetags.static import static
+from mainapp.models import WordLinks
 
 
 def main_view (request):
@@ -6,7 +9,22 @@ def main_view (request):
 
 
 def link_view (request):
-    return render(request, 'link_test.html', {})
+
+    context = {}
+
+    # print(WordLinks.objects.values())
+
+    context['words'] = list(WordLinks.objects.values())
+
+    # words = [{'word': word, 'ref': ref} for word, ref in WordLinks.objects.values()]
+
+    # context['words'] = words
+    # with open(static('links.WordLinks.objects.values()txt'), 'r') as file:
+    #     lines = file.readlines()
+    #     context['words'] = [{'word': word, 'ref': ref} for word, ref in map(lambda x: x.split(), lines)]
+
+    # print(context)
+    return render(request, 'link_test.html', context)
 
 
 def ad_view (request):
@@ -15,7 +33,7 @@ def ad_view (request):
 
 def vandalism_view (request):
     comments = [
-        {'author': 'Рандомный чел', 'text': 'Понятия не имею, кто это'},
+        {'author': 'Рандомный чел', 'text': 'Понятия не имею, кто это, но мне кажется, что ему меньше 18'},
         {'author': 'Жира', 'text': 'Я хачу пиццы'},
         {'author': 'Егор', 'text': 'Просто солнышко'},
     ]
